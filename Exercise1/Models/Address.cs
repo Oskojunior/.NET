29e1863c3acc14a3ffaf7bf78ed7088ea39e1254
Osko <ia.kowalczyk5@gmail.com>
2021-03-19 12:27:38 +0100

    Two previous tasks

---
+++ Exercise1/Models/Address.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise1.Models
+{
+    public class Address
+    {
+        [Display(Name = "Favourite Street")]
+        [Required(ErrorMessage = "This field is mandatory")]
+        public string Street { get; set; }
+        [Display(Name = "ZIPPO")]
+        [RegularExpression("[0-9]{2}-[0-9]{3}",ErrorMessage ="Not the Zip Code"), Required]
+        public string ZipCode { get; set; }
+        [Display(Name = "Town")]
+        [Required]
+        public string City { get; set; }
+        [Display(Name = "Numb-er")]
+        [StringLength(60,MinimumLength =1,ErrorMessage ="Not a house number"), Required]
+        public string Number { get; set; }
+
+    }
+}
