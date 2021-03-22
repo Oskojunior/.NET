9433b38520286b7eaa502e6d628b02d85b756cf8
Osko <ia.kowalczyk5@gmail.com>
2021-03-22 17:58:34 +0100

    FizzBuzz Web version

---
+++ FizzBuzz ver.2/Models/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_ver._2.Models
+{
+    public class FizzBuzz
+    {
+        [Range(1,1000), Required(ErrorMessage ="Number not found")]
+        public int Number { get; set; }
+        public string Outcome { get; set; }
+
+        public string Date { get; set; }
+        public void Calculate()
+        {
+            if (this.Number % 3 == 0 && this.Number % 5 == 0)
+                this.Outcome = "Otrzymano: FizzBuzz";
+            else if (this.Number % 3 == 0)
+                this.Outcome = "'Otrzymano: Fizz";
+            else if (this.Number % 5 == 0)
+                this.Outcome = "Otrzymano: Buzz";
+            else
+                this.Outcome = "Liczba: " + this.Number + " nie spełnia kryteriów Fizz / Buzz";
+        }
+    }
+}
---
+++ FizzBuzz ver.2/Pages/Privacy.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using FizzBuzz_ver._2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_ver._2.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        public FizzBuzz FizzBuzz { get; set; }
+        public List<FizzBuzz> FizzBuzzList { get; set; }
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+            var SessionAddress = HttpContext.Session.GetString("SessionAddress");
+
+            if (SessionAddress != null)
+            {
+                FizzBuzzList = JsonConvert.DeserializeObject<List<FizzBuzz>>(SessionAddress);
+
+            }
+            else
+            {
+                FizzBuzzList = new List<FizzBuzz>();
+            }
+            FizzBuzzList.Reverse();
+        }
+    }
+    
+}
